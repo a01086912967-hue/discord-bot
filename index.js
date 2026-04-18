d38b94c440b2e9123b3eb643b56838513f147710
a01086912967-hue <a01086912967@gmail.com>
2026-04-18 23:23:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ const { Client, GatewayIntentBits } = require('discord.js');
 console.log("TOKEN 있음?", process.env.TOKEN ? "YES" : "NO");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once('ready', () => {
-  console.log('봇 켜짐');
-  console.log(client.user.tag);
+  console.log('🔥 봇 켜짐');
+  console.log('👉', client.user.tag);
 });
 
 client.login(process.env.TOKEN)
-  .then(() => console.log("로그인 성공"))
-  .catch(err => console.log("로그인 실패:", err));
+  .then(() => console.log("✅ 로그인 성공"))
+  .catch(err => console.log("❌ 로그인 실패:", err));
 
 require('http').createServer((req, res) => {
   res.end('OK');
